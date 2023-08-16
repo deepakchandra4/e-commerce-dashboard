1123ed128eb899107d9c481380169b16bed1a8f9
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-08-16 11:16:42 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,14 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
+
 app.post("/register", async (req, res) => {
   const user = new User(req.body);
   let result = await user.save();
   result = result.toObject();
-  delete result.password;  //not to  show password
+  delete result.password; //not to  show password
   res.send(result);
 });
 
@@ -20,33 +23,71 @@ app.post("/login", async (req, res) => {
   if (req.body.email && req.body.password) {
     const user = await User.findOne(req.body).select("-password"); //to remove password
     if (user) {
+      Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          res.send({
+            result: "Something went wrong  , PLease try after sometime",
+          });
+        }
+        res.send({ user, auth: token });
+      });
       res.send(user);
     } else {
       res.send({ result: "No user found" });
     }
-  }else{
-    res.send({result : "No user found"});
+  } else {
+    res.send({ result: "No user found" });
   }
 });
 
-app.post('/add-product' , async (req , res) => {
-        let product = new Product(req.body);
-        let result = await product.save();
-        res.send(result);
-})
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.send(result);
+});
 
-app.get('/products' , async (req , res) => {
+app.get("/products", async (req, res) => {
   let products = await Product.find();
-  if(products.length > 0){
+  if (products.length > 0) {
     res.send(products);
-  }else{
-    res.send({result : "No Product found"});
+  } else {
+    res.send({ result: "No Product found" });
   }
-})
+});
 
-app.delete('/product/:id', async (req,res) => {
-    const result = await  Product.deleteOne({_id:req.params.id});
-    res.send(result)
-})
+app.delete("/product/:id", async (req, res) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
+  res.send(result);
+});
+
+app.get("/product/:id", async (req, res) => {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "NO Product found" });
+  }
+});
+
+app.put("/product/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    { id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(result);
+});
+
+app.get("/search/:key", async (req, res) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  res.send(result);
+});
 
 app.listen(4000, console.log("Port is running at 4000"));
